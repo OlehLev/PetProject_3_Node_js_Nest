d6ecb263e6aa667e1421393c273229168912e571
Oleh Levchenko <olehjurievich@gmail.com>
2023-05-02 16:25:08 +0300

    added work logic a controller and a service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
+import { AppController } from "./app.contoller";
+import { AppService } from "./app.service";
 
 @Module({
-    
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
