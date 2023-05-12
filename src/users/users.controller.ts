5f43ee07ca05164776b143775b310ad495f8fa35
Oleh Levchenko <olehjurievich@gmail.com>
2023-05-12 10:25:56 +0300

    added routes create and find users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { log } from 'console';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService){};
+
+    @Post()
+    create(@Body() userDto: CreateUserDto) {      
+        return this.usersService.createUser(userDto)
+    };
+
+    @Get()
+    getAllUsers() {
+        return this.usersService.getAllUsers()
+    }
+}
